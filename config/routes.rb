09a48c036f63c3bd8324e4c291fa5c434782dd48
TonyXie <txie145@gmail.com>
2014-05-16 13:26:21 -0700

    team design

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 InnovationFund::Application.routes.draw do
   root 'landing#index'
+  get '/application' => 'application#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
