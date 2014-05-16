a83308d2020bc8b16d61b98e03e3b79e901eeaa0
TonyXie <txie145@gmail.com>
2014-05-16 10:24:19 -0700

    added custom css theme

---
+++ README.md
@@ -0,0 +1,1 @@
+Innovation fund site
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 InnovationFund::Application.routes.draw do
+  root 'landing#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
